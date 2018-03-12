14731e035f58031de4b62063768c2a54007eb916
JandersonCRB <janderson@viajamont.com>
2018-03-12 15:18:29 -0300

    changed paperclip bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
 		path: ':class/:attachment/:id/:style/:filename',
 		s3_host_name: 's3-sa-east-1.amazonaws.com',
 		s3_credentials: {
-			bucket: 'test.montviagens.com',
+			bucket: ENV['BUCKET_NAME'],
 			access_key_id: ENV['AWS_ACCESS_KEY'],
 			secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
 			s3_region: 'sa-east-1'

98eae8fc44fbbd27f77f01bbbfe881aa1fdc0bb0
JandersonCRB <janderson@viajamont.com>
2018-04-03 11:33:07 -0300

    turned off gzip

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module MontExperiencesApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.assets.gzip = false
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
